62ed59f7432a1b0477d11af28c19bc7c68122a95
nazrul78 <nnakib78@gmail.com>
2025-03-09 12:44:36 +0600

    Added some variables

--- lib/src/service/local_storage_service.dart
+++ lib/src/service/local_storage_service.dart
@@ -1,1 +1,11 @@
-class LocalStorageService {}
+class LocalStorageService {
+  final abc = 2023;
+  final abc1 = 2023;
+  final abc2 = 2023;
+  final abc3 = 2023;
+  final abc4 = 2023;
+  final abc5 = 2023;
+  final abc6 = 2023;
+  final abc7 = 2023;
+  final abc8 = 2023;
+}
