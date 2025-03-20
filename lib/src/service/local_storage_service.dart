d78a23a8e82c129e204ccf102b92d26a18f72c20
nazrul78 <nnakib78@gmail.com>
2025-03-20 14:42:28 +0600

    Changed

--- lib/src/service/local_storage_service.dart
+++ lib/src/service/local_storage_service.dart
@@ -1,4 +1,11 @@
 class LocalStorageService {
   final abc = 2023;
   final abc1 = 2023;
+  final abc12 = 2023;
+  final abc10 = 2023;
+  final abc1452 = 2023;
+  final abc100 = 2023;
+  final abc1745274 = 2023;
+  final abc14274 = 2023;
+  final abc1474 = 2023;
 }
